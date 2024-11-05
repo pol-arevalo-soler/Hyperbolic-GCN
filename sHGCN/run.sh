d863c9ba1b797ad2cd36fb5eb2a9ada328863647
Pol Arévalo <parevalosoler@gmail.com>
2024-11-05 10:13:51 +0100

    First commit

---
+++ sHGCN/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+python3 train.py \
+    --task nc \
+    --dataset airport \
+    --model HGCN \
+    --lr 0.01 \
+    --dim 16 \
+    --num-layers 3 \
+    --act relu \
+    --bias 1 \
+    --dropout 0.0 \
+    --weight-decay 0.0 \
+    --manifold PoincareBall \
+    --log-freq 5 \
+    --cuda -1 \
+    --c None \
+    --r 0.0 \
+    --t 1.0 \
